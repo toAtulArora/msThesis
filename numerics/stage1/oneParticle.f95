04df300fc5a3c96a4afd183ebadf9d3d079d3e98
Atul Singh Arora <toatularora@gmail.com>
2015-11-02 11:48:10 +0530

    + ‘numerics/stage1/.#oneParticle.f95’ / ‘numerics/stage1/oneParticle.f95’

--- numerics/stage1/oneParticle.f95
+++ numerics/stage1/oneParticle.f95
@@ -5,14 +5,17 @@ program oneParticle
   real, parameter :: xMax=10, xMin=-10, dx=0.01, dt=0.001, tMax=10
   integer, parameter :: maxS=(xMax-xMin)/dx, maxT=tMax/dt
   
-  real, dimension(maxS,maxT) :: k1,k2,k3,k4
+  real, dimension(maxS,maxT) :: xc,k1,k2,k3,k4
   complex, dimension(maxS,maxT) :: x
   complex, dimension(maxS,maxT) :: psi
-  complex, dimension(maxS) :: m1,m2,m3,m4
+  complex, dimension(maxS) :: psic,m1,m2,m3,m4
 
   psi=0
+  x=0
+
+  psic = 
   
 contains
-
+  
   
 end program oneParticle
