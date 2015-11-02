d7851b391bf311c446306fe8570608d2105915bf
Atul Singh Arora <toatularora@gmail.com>
2015-11-02 08:40:58 +0530

    - ‘numerics/stage1/#oneParticle.f95#’ - ‘numerics/stage1/.#oneParticle.f95’ / ‘numerics/stage1/oneParticle.f95’

--- numerics/stage1/oneParticle.f95
+++ numerics/stage1/oneParticle.f95
@@ -2,7 +2,8 @@ program oneParticle
   use gnuplot_fortran
   implicit none
 
-  real, parameter :: xMax=-10, xMin=10, dx=0.01, dt=0.001
+  real, parameter :: xMax=10, xMin=-10, dx=0.01, dt=0.001, tMax=10
+  integer, parameter :: maxS=(xMax-xMin)/dx, maxT=tMax/dt
   real, dimension(maxI,3) :: x,k1,k2,k3,k4
   real, dimension(maxI,3) :: psi,m1,m2,m3,m4
   
