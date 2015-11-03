598b3e45ba51b14285774fcf2cdc18fe15287c7c
Atul Singh Arora <toatularora@gmail.com>
2015-11-03 15:17:24 +0530

    - ‘numerics/stage1/.#oneParticle.f95’ / ‘numerics/stage1/oneParticle.f95’

--- numerics/stage1/oneParticle.f95
+++ numerics/stage1/oneParticle.f95
@@ -62,7 +62,7 @@ contains
     qDelta=qPlus-q
     kineticPart=-hbar*hbar(psiPar(qPlus) + psiPar(qMinus) - 2*psiPar(q))/(qDelta*qDelta)
     potentialPart=V(q)*psiPar(q)
-    psiDot=(1/(0,1)hbar)*kineticPart*potentialPart
+    psiDot=(1/(0,1)hbar)*(kineticPart + potentialPart)
     
   end function psiDot
 
