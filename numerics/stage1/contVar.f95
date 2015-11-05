8b4357bbf8594472c4976a2cd31fdfc5091ff665
Atul Singh Arora <toatularora@gmail.com>
2015-11-05 18:41:14 +0530

    - ‘numerics/stage1/#contVar.f95#’ - ‘numerics/stage1/.#contVar.f95’ / ‘numerics/stage1/contVar.f95’

--- numerics/stage1/contVar.f95
+++ numerics/stage1/contVar.f95
@@ -14,6 +14,12 @@ module classContVar
 
   subroutine contVarInit(this)
     type(contVar) :: this
-    
+    real, dimension(size(this%
+    call spline(xPar,real(del2psiPar),rb,rc,rd,size(xPar))
+    !combine the cofficients to a complex # array
+    b=rb + (0,1)*ib
+    c=rc + (0,1)*ic
+    d=rd + (0,1)*id
+    call spline(xPar,real((0,-i)*del2psiPar),ib,ic,id,size(xPar)) !this funky thing is just to send the    
   end subroutine contVarInit
 end module classContVar
