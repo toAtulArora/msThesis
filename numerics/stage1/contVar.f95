df56fa95e276f6c8e38f712337a86a0c77b53afc
Atul Singh Arora <toatularora@gmail.com>
2015-11-05 19:16:49 +0530

    - ‘numerics/stage1/#contVar.f95#’ - ‘numerics/stage1/.#contVar.f95’ / ‘numerics/stage1/contVar.f95’

--- numerics/stage1/contVar.f95
+++ numerics/stage1/contVar.f95
@@ -30,10 +30,8 @@ module classContVar
   function contVarInterp(this,q)
     type(contVar) :: this
     real :: q
-
     real :: contVarInterp
-    
-    del2psiAtQ=ispline(q,xPar,real(del2psiPar),real(b),real(c),real(d),size(psiPar)) + (0,1)*ispline(q,xPar,real((0,-1)*del2psiPar),real((0,-1)*b),real((0,-1)*c),real((0,-1)*d),size(psiPar))
+    contVarInterp=ispline(q,this%x,real(this%f),real(this%b),real(this%c),real(this%d),size(psiPar)) + (0,1)*ispline(q,xPar,real((0,-1)*del2psiPar),real((0,-1)*b),real((0,-1)*c),real((0,-1)*d),size(psiPar))
   end subroutine contVarInterp
 
 end module classContVar
