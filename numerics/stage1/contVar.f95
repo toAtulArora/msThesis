22cdb153875437953699a089b53f2b7c114a48e7
Atul Singh Arora <toatularora@gmail.com>
2015-11-05 18:46:21 +0530

    - ‘numerics/stage1/.#contVar.f95’ / ‘numerics/stage1/contVar.f95’

--- numerics/stage1/contVar.f95
+++ numerics/stage1/contVar.f95
@@ -19,10 +19,12 @@ module classContVar
     !These are for holding the spline parameters temporarily
     real, dimension(size(this%x)) :: rb,rc,rd,ib,ic,id
     call spline(xPar,real(del2psiPar),rb,rc,rd,size(xPar))
+    call spline(this%x,real((0,-i)*this%f),ib,ic,id,size(this%x)) !this funky thing is just to send the
+
     !combine the cofficients to a complex # array
     b=rb + (0,1)*ib
     c=rc + (0,1)*ic
     d=rd + (0,1)*id
-    call spline(this%x,real((0,-i)*this%f),ib,ic,id,size(this%x)) !this funky thing is just to send the    
+
   end subroutine contVarInit
 end module classContVar
