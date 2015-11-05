2e1215d403e9356af3990cfae2c7baf6046fff11
Atul Singh Arora <toatularora@gmail.com>
2015-11-05 17:48:14 +0530

    - ‘numerics/stage1/.#contVar.f95’ / ‘numerics/stage1/contVar.f95’

--- numerics/stage1/contVar.f95
+++ numerics/stage1/contVar.f95
@@ -4,6 +4,11 @@ module classContVar
   public :: contVar, contVarInit, contVarInterp
 
   type contVar
-     complex, dimension(:) :: 
+     !the data points f(x)
+     complex, dimension(:) :: f
+     !the corresponding (x)
+     real, dimension(:) :: x
+     !the computed spline parameters
+     complex, dimension(:) :: b,c,d
   end type contVar
 end module classContVar
