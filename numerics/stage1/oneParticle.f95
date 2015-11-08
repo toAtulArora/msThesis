49755f6a7b9b4b9a66f595fc73b7cd45c0563387
Atul Singh Arora <toatularora@gmail.com>
2015-11-08 17:00:29 +0530

    / ‘numerics/stage1/oneParticle.f95’

--- numerics/stage1/oneParticle.f95
+++ numerics/stage1/oneParticle.f95
@@ -5,7 +5,8 @@ program oneParticle
   real, parameter :: xMax=10, xMin=-10, dx=0.01, dt=0.001, tMax=10, sigma=0.5
   real, parameter:: pi=3.14159265359,rootTwoPi=sqrt(2*pi),hbar=1
   integer, parameter :: maxS=(xMax-xMin)/dx, maxT=tMax/dt
-  
+
+  integer :: j
   ! real, dimension(maxS,maxT) :: qc
   ! complex :: k1,k2,k3,k4
   ! complex, dimension(maxS,maxT) :: q
