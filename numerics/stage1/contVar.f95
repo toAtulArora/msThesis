529af66d91469ff6b1824f3d49ad8155818afa47
Atul Singh Arora <toatularora@gmail.com>
2015-11-08 15:13:46 +0530

    - ‘numerics/stage1/.#contVar.f95’ / ‘numerics/stage1/contVar.f95’

--- numerics/stage1/contVar.f95
+++ numerics/stage1/contVar.f95
@@ -40,12 +40,12 @@ module classContVar
     this%d=rd + (0,1)*id
   end subroutine contVarInit
 
-  subroutine contVarInterp(this,q)
+  function contVarInterp(this,q)
     type(contVar) :: this
     !the value at which you want the value of f
     real :: q
     real :: contVarInterp
     contVarInterp=ispline(q,this%x,real(this%f),real(this%b),real(this%c),real(this%d),size(this%x)) + (0,1)*ispline(q,this%x,real((0,-1)*this%f),real((0,-1)*this%b),real((0,-1)*this%c),real((0,-1)*this%d),size(this%x))
-  end subroutine contVarInterp
+  end function contVarInterp
 
 end module classContVar
