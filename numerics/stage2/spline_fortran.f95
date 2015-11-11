97721f3d69e1ef871033df4171edb0753649264f
Atul Singh Arora <toatularora@gmail.com>
2015-11-11 14:04:58 +0530

    - ‘numerics/stage2/.#spline_fortran.f95’ / ‘numerics/stage2/spline_fortran.f95’

--- numerics/stage2/spline_fortran.f95
+++ numerics/stage2/spline_fortran.f95
@@ -264,4 +264,58 @@ contains
     iDel2 = 2*c(i) + 6*dx*d(i) !y(i) + dx*(b(i) + dx*(c(i) + dx*d(i)))
   end function iDel2
 
+  function iDel2(u, x, y, b, c, d, n)
+    !======================================================================
+    ! function iDel2 evaluates the second derivative using cubic spline interpolation at point z
+    ! iDel2 = y(i)+b(i)*(u-x(i))+c(i)*(u-x(i))**2+d(i)*(u-x(i))**3
+    ! where  x(i) <= u <= x(i+1)
+    !----------------------------------------------------------------------
+    ! input..
+    ! u       = the abscissa at which the spline is to be evaluated
+    ! x, y    = the arrays of given data points
+    ! b, c, d = arrays of spline coefficients computed by spline
+    ! n       = the number of data points
+    ! output:
+    ! iDel2 = interpolated value at point u
+    !=======================================================================
+    implicit none
+    real ::  iDel2
+    !double precision iDel2
+    integer n
+    !double precision  u, x(n), y(n), b(n), c(n), d(n)
+    real ::  u, x(n), y(n), b(n), c(n), d(n)
+    integer i, j, k
+    !double precision dx
+    real :: dx
+
+    ! if u is ouside the x() interval take a boundary value (left or right)
+    if(u <= x(1)) then
+       iDel2 = y(1)
+       return
+    end if
+    if(u >= x(n)) then
+       iDel2 = y(n)
+       return
+    end if
+
+    !*
+    !  binary search for for i, such that x(i) <= u <= x(i+1)
+    !*
+    i = 1
+    j = n+1
+    do while (j > i+1)
+       k = (i+j)/2
+       if(u < x(k)) then
+          j=k
+       else
+          i=k
+       end if
+    end do
+    !*
+    !  evaluate spline interpolation
+    !*
+    dx = u - x(i)
+    iDel2 = 2*c(i) + 6*dx*d(i) !y(i) + dx*(b(i) + dx*(c(i) + dx*d(i)))
+  end function iDel2
+  
 end module spline_fortran
