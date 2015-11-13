939450b270441a2cea35e8ea042a3109d6c71b65
Atul Singh Arora <toatularora@gmail.com>
2015-11-13 08:25:34 +0530

    / ‘numerics/stage2/oneParticle.f95’

--- numerics/stage2/oneParticle.f95
+++ numerics/stage2/oneParticle.f95
@@ -122,8 +122,8 @@ program oneParticle
      ! end do
      if (mod(timeStep,100)==0) then
         ! if (mod(timeStep,maxT/10)==0) then
-        call nextPlot2d(x(2:maxS-1),Varray(2:maxS-1))
-        ! call nextPlot2d(x(2:maxS-1),abs(psi(2:maxS-1,timeStep)))
+        !call nextPlot2d(x(2:maxS-1),Varray(2:maxS-1))
+        call nextPlot2d(x(2:maxS-1),abs(psi(2:maxS-1,timeStep)))
         !       call nextPlot2d( (/  ( tFi(j),j=1,timeStep) /) , q(1,1:timeStep)   )
 
         !qTemp=(/ (q(j,1:timeStep),j=1,ensambleSize) /)
